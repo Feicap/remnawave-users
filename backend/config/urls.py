6eb4eb89df54768f47827df36cdc517f043b8f6a
Feicap <superrenjer2@yandex.ru>
2026-02-14 00:08:00 +0400

    Expose django prometheus metrics under /api/metrics

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -3,6 +3,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path("", include("django_prometheus.urls")),
+    path("api/", include("django_prometheus.urls")),
     path("admin/", admin.site.urls),
     path("api/", include("telegram_auth.urls")),
 ]
